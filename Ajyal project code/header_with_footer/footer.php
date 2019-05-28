c3578ba59f3f159a8978f5779727e07c31dfa40c
MohammedMohsen1 <51117120+MohammedMohsen1@users.noreply.github.com>
2019-05-28 14:10:30 +0300

    Add files via upload

---
+++ Ajyal project code/header_with_footer/footer.php
@@ -0,0 +1,31 @@
+=<footer class="m-auto">
+    <div class="footer-top" style="  position: marker;right: 0 ; bottom: 0 ; left: 0 ; text-align: right ; ">
+        <div class="container w-100 h-100  mr-auto ml-auto  mt-5">
+            <div class="row m-auto">
+                <div id="contact us" class="footer-contact wow fadeInDown m-auto">
+                    <h3>تواصل معنا</h3>
+                    <p>النصر مقابل جامعة القدس المفتوحة <i class="fas fa-map-marker-alt"></i></p>
+                    <p>رقم الهاتف : (0039) 333 12 68 347 <i class="fas fa-phone"></i></p>
+                    <p>البريد الإلكتروني : <a href="mailto:hello@domain.com">Ajyal@gmail.com</a><i class="fas fa-envelope"></i></p>
+                </div>
+                
+                <div class="footer-social wow fadeInUp mr-auto ml-auto">
+                    <h3>تابعنا</h3>
+                    <p class="m-auto">
+                        <a class="m-auto" href="#"><i class="fab fa-facebook"></i></a>
+                        <a class="mr-1" href="#"><i class="fab fa-instagram"></i></a>
+
+                    </p>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</footer>
+
+<script src="../../Layout%20And%20Style/js/jQuery%203.4.0.js"></script>
+<script src="../../Layout%20And%20Style/js/popper.js"></script>
+<script src="../../Layout%20And%20Style/js/bootstrap.js"></script>
+</body>
+</html>';
---
+++ Ajyal project code/header_with_footer/header.php
@@ -0,0 +1,50 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="../../Layout%20And%20Style/images/ajyal_logo.png">
+    <link rel="stylesheet" href="../../Layout%20And%20Style/css/style.css">
+    <link rel="stylesheet" href="../../Layout%20And%20Style/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../Layout%20And%20Style/css/bootstrap.css">
+    <link rel="stylesheet" href="../../Layout%20And%20Style/icons/fontawesome-free-5.8.1-web/css/all.css">
+    <title>Ajyal</title>
+    <style>
+        html {
+            direction: rtl;
+        }
+
+        section {
+            direction: rtl;
+            text-align: right;
+        }
+    </style>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-md bg-dark navbar-dark ">
+    <a class="navbar-brand" href="#">
+        <img src="../../Layout%20And%20Style/images/ajyal_logo.png" alt="Logo" style="width:120px;">
+    </a>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav ">
+            <li class="nav-item">
+                <a class="nav-link" href="#">الرئيسية</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">خدماتنا</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">من نحن</a>
+            </li>
+        </ul>
+    </div>
+    <a class="btn  btn-sm btn-success text-white" type="button" href="#" >تسجيل الدخول</a>
+</nav>
